b82bf8bba63b6113ff3ead2b42bd184eea632f87
Geneve Estrada <geneve.estrada@outlook.com>
2019-04-25 18:34:02 +1000

    Geneve: Progress fragment

--- app/src/main/java/com/example/hellopanda/fragments/ProgressFragment.java
+++ app/src/main/java/com/example/hellopanda/fragments/ProgressFragment.java
@@ -7,15 +7,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.hellopanda.R;
 
 public class ProgressFragment extends Fragment {
 
+    View myFragment;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_progress, container, false);
+        myFragment = inflater.inflate(R.layout.fragment_progress, container, false);
 
+        //TO DO: get highest score and replace in "Hi" -> String.valueOf(score);
+
+
+        TextView highestScore = (TextView)myFragment.findViewById(R.id.highestScore);
+        highestScore.setText("Hi");
+
+        //if visibility is 0
+        //
+        //TO DO: if score is = ___
+
+        return myFragment;
     }
 }
